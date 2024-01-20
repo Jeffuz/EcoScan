97b23d85bb799ae5584705b05d875277cdeb056e
Jeffuz <jeffzhang0049@gmail.com>
2024-01-20 00:24:52 -0800

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+/* 
+To add more paths for the paging, follow:
+* import cruzhacks2024 from ...;
+* <Route path='/cruzhacks2024' element={<cruzhacks2024 />} />
+*/
 
 function App() {
   return (
     <>
-      <div>
-        
-      </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   );
 }
 
 export default App;
+
